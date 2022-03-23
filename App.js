aa675cff735997b06b8467f69e10ec0faf31c534
leticia-f <leticia.fonseca.1106@gmail.com>
2022-03-23 19:29:37 -0300

    mudanças de cor e botão - 23.03

--- App.js
+++ App.js
@@ -1,10 +1,13 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Button } from 'react-native';
+import { Linking } from 'react-native-web';
 
-export default function App() {
+export default function App() { 
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+    <View style={styles.container}> {/* view = <div> */}
+      <Text style={styles.text}></Text> {/* text = <p> */}
+      <Button title="gitHub"
+              onPress={()=>{Linking.openURL('https://github.com/leticia-f')}}/>
       <StatusBar style="auto" />
     </View>
   );
@@ -13,8 +16,12 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#6393A6',
     alignItems: 'center',
     justifyContent: 'center',
   },
+  text: {
+    fontSize: 30,
+    color: '#733B36',
+  }
 });
