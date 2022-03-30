1a8a9e9b3480bc89db1f980c5b9f6607913ab663
leticia-f <leticia.fonseca.1106@gmail.com>
2022-03-30 18:31:25 -0300

    subindo contador - 30.03

--- App.js
+++ App.js
@@ -1,14 +1,18 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View, Button } from 'react-native';
-import { Linking } from 'react-native-web';
+import { React, useState } from 'react';
+import { StyleSheet, Text, View, Button, Linking } from 'react-native';
 
-export default function App() { 
+export default function App() {
+  const [counter, setCounter] = useState(0); {/* usestate - props (não dá para mudar) - valor do contador começa com 0 */ }
+
+  const onClickHandler = () => {
+    setCounter(counter + 1)
+  }
+
   return (
-    <View style={styles.container}> {/* view = <div> */}
-      <Text style={styles.text}></Text> {/* text = <p> */}
-      <Button title="gitHub"
-              onPress={()=>{Linking.openURL('https://github.com/leticia-f')}}/>
-      <StatusBar style="auto" />
+    <View style={styles.container}>
+      <Text style={styles.text}> {counter * 5} </Text> {/* text = <p> */}
+      <Button style={styles.button} title="Adicionar 5" onPress={onClickHandler} />
+      <Text style={styles.text}> Você clicou {counter} vezes. </Text>
     </View>
   );
 }
@@ -23,5 +27,9 @@ const styles = StyleSheet.create({
   text: {
     fontSize: 30,
     color: '#733B36',
-  }
+  },
+  button: {
+    color: '#733B36',
+    padding: 20px,
+  },
 });
