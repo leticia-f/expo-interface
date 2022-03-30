5f8cbb8a4d289654964f0efb448159d97b292592
leticia-f <leticia.fonseca.1106@gmail.com>
2022-03-30 19:09:51 -0300

    editando direções com flex - 30.03

--- App.js
+++ App.js
@@ -4,7 +4,7 @@ import { StyleSheet, Text, View, Button, Linking } from 'react-native';
 // state é mutável e o props não é
 
 export default function App() {
-  const [atualizado,setAtualizado] = useState('me atualiza')
+  const [atualizado, setAtualizado] = useState('me atualiza')
 
   const onClickHandler = () => {
     setAtualizado('atualizei')
@@ -12,26 +12,50 @@ export default function App() {
 
   return (
     <View style={styles.container}>
-      <Text style={styles.text}> tá sabendo da fofoca? </Text>
-      <Text style={styles.text}> {atualizado} </Text>
-      <Button style={styles.button} title="me conta" onPress={onClickHandler} />
+      <View style={styles.view1}>
+        <Text style={styles.text}> 1 </Text>
+      </View>
+      <View style={styles.view2}>
+        <Text style={styles.text}> 2 </Text>
+      </View>
+      <View style={styles.view3}>
+        <Text style={styles.text}> 3 </Text>
+      </View>
     </View>
   );
 }
+
+{/* a prioridade é mudar as direções*/}
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#6393A6',
-    alignItems: 'center',
+    alignItems: 'stretch', /* stretch serve para preencher 100% */
     justifyContent: 'center',
+    flexDirection: 'column',
   },
   text: {
     fontSize: 30,
-    color: '#733B36',
+    color: '#233B36',
+    width:200,
   },
-  button: {
-    color: '#733B36',
-    padding: 20,
+  view1: {
+    flex:1,
+    backgroundColor: '#2384A1',
+    alignItems:'center',
+    justifyContent:'center',
+  },
+  view2: {
+    flex:2,
+    backgroundColor: '#5384A1',
+    alignItems:'center',
+    justifyContent:'center',
+  },
+  view3: {
+    flex:3,
+    backgroundColor: '#7384A1',
+    alignItems:'center',
+    justifyContent:'center',
   },
 });
