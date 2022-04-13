7fa29d15151b11718bb1b81e267d5a89ec669467
leticia-f <leticia.fonseca.1106@gmail.com>
2022-04-13 19:26:19 -0300

    API de personagens, map + object - 13.04

--- App.js
+++ App.js
@@ -1,128 +1,77 @@
-import {React,useState} from 'react';
+import React, { useState } from 'react';
 import {
   StyleSheet,
   View,
   Text,
-  Button,
-  Linking,
+  ScrollView,
+  RefreshControl,
 } from 'react-native';
-{/* STATE É MUTAVEL E O PROPS NÃO É*/}
-const App = () => {
-  const [atualizado,setAtualizado]= useState('ME ATUALIZA')
-  const onClickHandler= ()=>{
-    setAtualizado('Atualizei, li no twitter')
-  }
-  return (
-    <View style={styles.body}>
-      <View style={styles.row}>
-        <View style={styles.view1}>
-          <Text style={styles.text}> 1 </Text>
-        </View>
 
-        <View style={styles.view2}>
-          <Text style={styles.text}> 2 </Text>
-        </View>
-        
-        <View style={styles.view3}>
-          <Text style={styles.text}> 3 </Text>
-          </View>
-      </View>
-      <View style={styles.row}>
-        <View style={styles.view4}>
-          <Text style={styles.text}> 4 </Text>
-        </View>
-      
-        <View style={styles.view5}>
-          <Text style={styles.text}> 5 </Text>
-        </View>
-        
-        <View style={styles.view6}>
-          <Text style={styles.text}> 6 </Text>
-        </View>
-      </View>
-      <View style={styles.big_row}>
-        <View style={styles.view7}>
-          <Text style={styles.text}> 7 </Text>
-        </View>
-      </View>
-      
-      </View>
+const App = () => {
+
+  const [Items, setItems] = useState([
+    {key: 1, personagem: 'Ai Ohto',         midia: 'Wonder Egg Priority'},
+    {key: 2, personagem: 'Kyoka Jiro',      midia: 'Boku no Hero'},
+    {key: 3, personagem: 'Gin Akutagawa',   midia: 'Bungo Stray Dogs'},
+    {key: 4, personagem: 'Chuuya Nakahara', midia: 'Bungo Stray Dogs'},
+    {key: 5, personagem: 'Sakura Ogami',    midia: 'Trigger Happy Havoc'},
+    {key: 6, personagem: 'Mahiru Koizumi',  midia: 'Goodbye Despair'},
+    {key: 7, personagem: 'Yanfei',          midia: 'Genshin Impact'},
+    {key: 8, personagem: 'Albedo',         midia: 'Genshin Impact'},
+    {key: 9, personagem: 'Seele Vollerei',  midia: 'Honkai Impact'},
+    {key: 10, personagem: 'Shinobu Kocho',   midia: 'Demon Slayer'},
+  ]);
+  const [Refreshing, setRefreshing] = useState(false);
+
+  const onRefresh = () => {
+    setRefreshing(true);
+    setItems([...Items, { key: 69, item: 'Item 69' }]);
+    setRefreshing(false);
+  }
+  {/*ESTE CÓDIGO SÓ ATUALIZA PELO CELULAR, ELE ADICIONA O ITEM 69 QUANDO FOR RECOMPILADO*/}
+  return (
+    <ScrollView
+      style={styles.body}
+      refreshControl={
+        <RefreshControl
+          refreshing={Refreshing}
+          onRefresh={onRefresh}
+          colors={['#449f']}
+        />
+      }
+    >
+      {
+        Items.map((object) => {
+          return (
+            <View style={styles.item} key={object.key}>
+              <Text style={styles.text}>
+                personagem fav: {object.personagem} <br/>
+                anime/jogo: {object.midia}</Text>
+            </View>
+          )
+        })
+      }
+    </ScrollView>
   );
 };
-
-{/* A PRIORIDADE É MUDAR AS DIREÇÕES*/}
 
 const styles = StyleSheet.create({
   body: {
     flex: 1,
-    backgroundColor: '#0726D9',
-    alignItems: 'stretch',
-    /* stretch serve para preencher 100% */
     flexDirection: 'column',
-    justifyContent: 'flex-start'
+    backgroundColor: '#fff',
   },
-  row: {
-    flexDirection: 'row',
-    backgroundColor: '#ffffff',
+  item: {
+    margin: 10,
+    backgroundColor: '#449f',
+    justifyContent: 'center',
     alignItems: 'center',
-    justifyContent: 'center',
-  },
-  
-  big_row: {
-    flex: 1,
-    flexDirection: 'row',
-    backgroundColor: '#ffffff',
-    alignItems: 'stretch',
-    justifyContent: 'center',
   },
   text: {
-    color: '#ffffff',
-    fontSize: 20,
-    fontStyle:'italic',
+    color: '#fff',
+    fontSize: 30,
+    fontStyle: 'italic',
     margin: 10,
-    width: 100,
-  },
-  view1:{
-    flex:1,
-    backgroundColor:'#00ffff',
-    alignItems: 'center',
-    justifyContent:'center',
-  },
-  view2:{
-    flex:1,
-    backgroundColor:'#ff00ff',
-    alignItems: 'center',
-    justifyContent:'center',
-  },
-  view3:{
-    flex:1,
-    backgroundColor:'#ffff00',
-    alignItems: 'center',
-    justifyContent:'center',
-  },
-  view4:{
-    flex:1,
-    backgroundColor:'#ff0000',
-    alignItems: 'center',
-    justifyContent:'center',
-  },
-  view5:{
-    flex:1,
-    backgroundColor:'#0fff0f',
-    alignItems: 'center',
-    justifyContent:'center',
-  },
-  view6:{
-    flex:1,
-    backgroundColor:'#ffffff',
-    alignItems: 'center',
-    justifyContent:'center',
-  },
-  view7:{
-    flex:1,
-    backgroundColor:'#0000ff',
-    alignItems: 'center',
-    justifyContent:'center',
   },
 });
 
