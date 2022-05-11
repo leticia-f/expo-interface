1e74564adadc46f4672f6f209b63f857ee769e19
leticia-f <leticia.fonseca.1106@gmail.com>
2022-05-11 19:22:43 -0300

    view/continuação api ghibli - 11.05

--- App.js
+++ App.js
@@ -1,48 +1,81 @@
-import React, { useState, useEffect } from 'react';
-import {
-  StyleSheet,
-  View,
-  Text,
-  ScrollView,
-  RefreshControl,
-  SafeAreaView,
-  FlatList,
-  Image,
-} from 'react-native';
+import React, { useEffect, useState } from 'react'
+import { SafeAreaView, View, FlatList, Image, Text, StyleSheet } from 'react-native'
 
 export default function App() {
 
-  const [allMovies, setAllMovies] = useState([]) // chamar função para mudar
+  const [allMovies, setAllMovie] = useState([])
 
   useEffect(() => {
+
+    // fetch busca os dados no site
+    // then response vai transformar resposta em um json
+    // then data vai formatar os dados listando/mapeando o array
 
     fetch('https://ghibliapi.herokuapp.com/films')
       .then(response => response.json())
       .then(data => {
         data.forEach(movie => {
-          console.log(movie);
-          setAllMovies(movie)
+          console.log(movie.title);
+          console.log(movie.original_title)
+          console.log(movie.image)
+          console.log(movie.description)
+          console.log(movie.release_date)
+          console.log(movie.director)
         })
-      })
-      .catch(err => {
-        console.log(err);
+
+        setAllMovie(data); // usando data em vez de movie porque o arquivo/resposta
+        // do .then é uma variável chamada data, movie é no map/forEach (já foi fechado)
+
+      }).catch(err => {
+        console.log(err)
       })
 
   }, [])
 
   return (
-    <View>
-      <Flatlist
+
+    <View style={styles.body}>
+      {/* flatlist exibe uma sequência de imagens */}
+
+      <Text style={styles.titulo} > Studio Ghibli </Text>
+      <FlatList
         data={allMovies}
-        keyExtractor={(movie) => movie.title}
-        renderItem={MovieShow}
+        renderItem={({ item }) =>  // atributo e item nos parênteses, respectivamente
+          // item (objeto) representa movie
+          // chave dentro de parênteses - desestruturação
+          <View>
+            <Text> {item.title} </Text> {/* puxar atributo, obj chamado item */}
+            <Text> {item.original_title} </Text>
+            <Image
+              style={{ width: 196, height: 294 }}
+              source={{ uri: item.image }} 
+              /> {/* uri é local */}
+            <Text> {item.description} </Text>
+            <Text> Ano de lançamento: {item.release_date} </Text>
+            <Text style={styles.end} > Direção: {item.director} </Text>
+          </View>
+        }
       />
+
     </View>
+
   )
-
 }
 
-function MovieShow(item){
-  const{title,original_title,banner,description,director,release_date} = item.item
+function MovieShow(item) {
+  const { title, description, image } = item.item
 }
 
+const styles=StyleSheet.create({
+  body:{
+    backgroundColor: '#FFF',
+    alignItems: 'center',
+  },
+  titulo:{
+    fontSize: 40,
+    padding: 20,
+  },
+  end:{
+    paddingBottom: 20,
+  },
+});
