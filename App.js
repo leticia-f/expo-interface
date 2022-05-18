929b93a5cdf1fe209d2fd556a94b247aa91523f0
leticia-f <leticia.fonseca.1106@gmail.com>
2022-05-18 19:30:10 -0300

    fontes - 18.05

--- App.js
+++ App.js
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from 'react'
 import { SafeAreaView, View, FlatList, Image, Text, StyleSheet } from 'react-native'
-
+import { styles } from './components/meusEstilos/styleRN'
 export default function App() {
 
   const [allMovies, setAllMovie] = useState([])
@@ -15,12 +15,12 @@ export default function App() {
       .then(response => response.json())
       .then(data => {
         data.forEach(movie => {
-          console.log(movie.title);
-          console.log(movie.original_title)
-          console.log(movie.image)
-          console.log(movie.description)
-          console.log(movie.release_date)
-          console.log(movie.director)
+          // console.log(movie.title);
+          // console.log(movie.original_title)
+          // console.log(movie.image)
+          // console.log(movie.description)
+          // console.log(movie.release_date)
+          // console.log(movie.director)
         })
 
         setAllMovie(data); // usando data em vez de movie porque o arquivo/resposta
@@ -44,14 +44,14 @@ export default function App() {
           // item (objeto) representa movie
           // chave dentro de parênteses - desestruturação
           <View>
-            <Text> {item.title} </Text> {/* puxar atributo, obj chamado item */}
-            <Text> {item.original_title} </Text>
+            <Text style={styles.textos}> {item.title} </Text> {/* puxar atributo, obj chamado item */}
+            <Text style={styles.textos}> {item.original_title} </Text>
             <Image
               style={{ width: 196, height: 294 }}
               source={{ uri: item.image }} 
               /> {/* uri é local */}
-            <Text> {item.description} </Text>
-            <Text> Ano de lançamento: {item.release_date} </Text>
+            <Text style={styles.textos}> {item.description} </Text>
+            <Text style={styles.textos}> Ano de lançamento: {item.release_date} </Text>
             <Text style={styles.end} > Direção: {item.director} </Text>
           </View>
         }
@@ -65,17 +65,3 @@ export default function App() {
 function MovieShow(item) {
   const { title, description, image } = item.item
 }
-
-const styles=StyleSheet.create({
-  body:{
-    backgroundColor: '#FFF',
-    alignItems: 'center',
-  },
-  titulo:{
-    fontSize: 40,
-    padding: 20,
-  },
-  end:{
-    paddingBottom: 20,
-  },
-});
---
+++ components/meusEstilos/styleRN.js
@@ -0,0 +1,28 @@
+import { StyleSheet } from "react-native";
+export const styles=StyleSheet.create({
+    body:{
+      backgroundColor: '#3A585C',
+      alignItems: 'center',
+    },
+    titulo:{
+      fontSize: 40,
+      padding: 20,
+      color: '#FFF',
+      fontFamily: 'CormorantGaramond-Regular',
+    },
+    textos:{
+      fontSize: 15,
+      paddingLeft: 20,
+      paddingRight: 20,
+      color: '#FFF',
+      fontFamily: 'CormorantGaramond-Light',
+    },
+    end:{
+      fontSize: 15,
+      paddingLeft: 20,
+      paddingRight: 20,
+      paddingBottom: 20,
+      color: '#FFF',
+      fontFamily: 'CormorantGaramond-Light',
+    },
+  });
